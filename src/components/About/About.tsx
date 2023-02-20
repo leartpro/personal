d7efe691c381d6ce6588e0ef3056ea570d4cb4b0
leartpro <lenni.protte@gmx.de>
2023-02-20 18:41:05 +0100

    update blog and about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,10 +6,26 @@ const About = () => {
         <section id={"about"}>
             <h1>About Me</h1>
             <Typography>
-                Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Sed ac nulla vitae turpis lobortis convallis in vel leo.
-                Fusce ut metus massa. Proin quis convallis massa, ut congue nisl.
-                Vivamus sollicitudin sagittis augue.
+                Hi, I'm Lennart - an open-minded student with a fascination for programming and software development.
+                For me, programming is more than just a hobby -
+                it's a way to express my creativity and explore the limits of what's possible.
+
+                I'm interested in engaging with new technologies
+                and the algorithmic question and developing applications that make life easier
+                and better for all of us. With expertise in various programming languages
+                and hands-on experience in software development and design,
+                I strive to find creative solutions that make the world a little bit better.
+
+                When I'm not sitting at the computer, I like to spend my free time with sports, friends and a good book.
+                In the future I would like to deepen my knowledge and skills
+                and therefore I will start studying General Computer Science.
+                During my time at school I was already able to gain experience,
+                for example in internships and participation in the German National Computer Science Competition.
+
+                I am always looking for new challenges and projects where I can use my skills
+                and enthusiasm for programming. So if you have an exciting project or just want to talk,
+                I look forward to hearing from you!
+
             </Typography>
         </section>
     );
