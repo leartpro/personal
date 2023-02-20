44f0bc342bf5448b0425963147dbbd20b76e967e
leartpro <lenni.protte@gmx.de>
2023-02-20 19:12:26 +0100

    update about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Typography} from "@mui/material";
+import axios from 'axios';
 
 const About = () => {
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        axios.get('./Content/About.txt').then((response) => {
+            setText(response.data);
+        });
+    }, []);
+
+    const emphasizedWords = ['programming', 'software development', 'technologies', 'algorithmic', 'creative solutions'];
+
     return (
-        <section id={"about"}>
+        <section id="about">
             <h1>About Me</h1>
-            <Typography>
-                Hi, I'm Lennart - an open-minded student with a fascination for programming and software development.
-                For me, programming is more than just a hobby -
-                it's a way to express my creativity and explore the limits of what's possible.
-
-                I'm interested in engaging with new technologies
-                and the algorithmic question and developing applications that make life easier
-                and better for all of us. With expertise in various programming languages
-                and hands-on experience in software development and design,
-                I strive to find creative solutions that make the world a little bit better.
-
-                When I'm not sitting at the computer, I like to spend my free time with sports, friends and a good book.
-                In the future I would like to deepen my knowledge and skills
-                and therefore I will start studying General Computer Science.
-                During my time at school I was already able to gain experience,
-                for example in internships and participation in the German National Computer Science Competition.
-
-                I am always looking for new challenges and projects where I can use my skills
-                and enthusiasm for programming. So if you have an exciting project or just want to talk,
-                I look forward to hearing from you!
-
+            <Typography style={{ wordWrap: 'break-word'}}>
+                {text.split(' ').map((word, index) => {
+                    const delay = index * 80;
+                    const emphasized = emphasizedWords.includes(word.toLowerCase());
+                    return (
+                        <span
+                            key={index}
+                            style={{
+                                animation: `typing 0.8s ease-out ${delay}ms both`,
+                                animationDelay: `${delay}ms`,
+                                color: emphasized ? '#2e7d32' : 'inherit',
+                                fontWeight: emphasized ? 'bold' : 'normal',
+                                textDecoration: emphasized ? 'underline' : 'none',
+                                textUnderlineOffset: emphasized ? '2px' : 'none',
+                            }}
+                        >
+              {word}{'\u00A0'}
+            </span>
+                    );
+                })}
             </Typography>
+            <style>
+                {`
+          @keyframes typing {
+            from { width: 0; }
+            to { width: 100%; }
+          }
+        `}
+            </style>
         </section>
     );
 }
+
 
 export default About;
