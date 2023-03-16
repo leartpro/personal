33c76b2d1c11dd7750acc38af15c6ba89d91fb20
leartpro <lenni.protte@gmx.de>
2023-03-16 17:24:35 +0100

    update JourneyGraph.tsx

--- src/components/MyJourney/JourneyGraph.tsx
+++ src/components/MyJourney/JourneyGraph.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {Gitgraph, Orientation, templateExtend, TemplateName} from "@gitgraph/react";
 
-function JourneyGraph() {
 
-    var customTagStyle = {
-        bgColor: 'orange',
-        strokeColor: 'orange',
-    };
+function JourneyGraph() {
+    const colors = ["gray", "green", "gold", "dodgerblue", "steelblue", "slateblue", "blueviolet", "darkmagenta", "darkslateblue", "navy", "indigo"]
+
+    function getCustomHeaderStyle(index: number) {
+        return {
+            color: colors[index + 3],
+            strokeColor: 'transparent',
+            bgColor: 'transparent',
+            font: '',
+            borderRadius: 0,
+            pointerWidth: 0,
+        }
+    }
 
     let options = {
-        //mode: Mode.Compact,
         orientation: Orientation.VerticalReverse,
         template: templateExtend("metro" as TemplateName, {
-            colors: ["gray", "green", "gold", "dodgerblue", "steelblue", "slateblue", "blueviolet", "darkmagenta", "darkslateblue", "navy"],
+            colors: colors,
             branch: {
                 label: {
                     display: false,
@@ -26,74 +33,138 @@ function JourneyGraph() {
             },
         })
     };
-
     return (
             <Gitgraph options={options}>
                 {gitgraph => {
                     const master = gitgraph.branch("master");
                     master.commit("");
-                    let release = gitgraph.branch("release").commit("");
-                    let develop = gitgraph.branch("develop").commit("");
+                    let release = gitgraph.branch({
+                        name: 'release',
+                        from: master,
+                    }).commit("");
+                    let develop = gitgraph.branch({
+                        name: 'develop',
+                        from: master,
+                    }).commit("");
+                    let tictactoe = gitgraph.branch({
+                        name: 'tictactoe',
+                        from: develop,
+                    }).commit("");
                     gitgraph.tag({
                         name: 'TicTacToe:',
-                        style: customTagStyle,
+                        style: getCustomHeaderStyle(0),
                     });
-                    let tictactoe = gitgraph.branch("tictactoe");
                     tictactoe.commit("skill learned in tictactoe");
-                    develop.merge(tictactoe);
+                    develop.merge({
+                        branch: tictactoe,
+                        commitOptions: {
+                            subject: ' ',
+                        },
+                    });
+                    let quap = gitgraph.branch({
+                        name: 'quap',
+                        from: develop,
+                    }).commit("");
                     gitgraph.tag({
                         name: 'Quap:',
-                        style: customTagStyle,
+                        style: getCustomHeaderStyle(1),
                     });
-                    let quap = gitgraph.branch("quap");
                     quap.commit("skill learned in quap");
-                    develop.merge(quap);
+                    develop.merge({
+                        branch: quap,
+                        commitOptions: {
+                            subject: ' ',
+                        },
+                    });
+                    let littSQL = gitgraph.branch({
+                        name: 'littsql',
+                        from: develop,
+                    }).commit("");
                     gitgraph.tag({
                         name: 'littSQL:',
-                        style: customTagStyle,
+                        style: getCustomHeaderStyle(2),
                     });
-                    let littSQL = gitgraph.branch("littSQL");
                     littSQL.commit("skill learned in littSQL");
-                    develop.merge(littSQL);
+                    develop.merge({
+                        branch: littSQL,
+                        commitOptions: {
+                            subject: ' ',
+                        },
+                    });
+                    let sudoku = gitgraph.branch({
+                        name: 'sudoku',
+                        from: develop,
+                    }).commit("");
                     gitgraph.tag({
                         name: 'Sudoku:',
-                        style: customTagStyle,
+                        style: getCustomHeaderStyle(3),
                     });
-                    let sudoku = gitgraph.branch("sudoku");
                     sudoku.commit("skill learned in sudoku");
-                    develop.merge(sudoku);
+                    develop.merge({
+                        branch: sudoku,
+                        commitOptions: {
+                            subject: ' ',
+                        },
+                    });
+                    let bwinf411 = gitgraph.branch({
+                        name: 'bwinf 41.1',
+                        from: develop,
+                    }).commit("");
                     gitgraph.tag({
                         name: 'Bwinf 41.1:',
-                        style: customTagStyle,
+                        style: getCustomHeaderStyle(4),
                     });
-                    let bwinf411 = gitgraph.branch("bwinf 41.1");
                     bwinf411.commit("skill learned in bwinf 41.1");
-                    develop.merge(bwinf411);
-                    gitgraph.tag({
-                        name: 'Chess4Player:',
-                        style: customTagStyle,
+                    develop.merge({
+                        branch: bwinf411,
+                        commitOptions: {
+                            subject: ' ',
+                        },
                     });
                     let chess4player = gitgraph.branch({
-                        name: "chess4player",
+                        name: 'chess4player',
                         from: develop,
+                    }).commit("");
+                    gitgraph.tag({
+                        name: 'Chess4Player:',
+                        style: getCustomHeaderStyle(5),
                     });
                     chess4player.commit("skill learned in chess4player");
-                    develop.merge(chess4player);
+                    develop.merge({
+                        branch: chess4player,
+                        commitOptions: {
+                            subject: ' ',
+                        },
+                    });
+                    let bwinf412 = gitgraph.branch({
+                        name: 'bwinf 41.2',
+                        from: develop,
+                    }).commit("");
                     gitgraph.tag({
                         name: 'Bwinf 41.2:',
-                        style: customTagStyle,
+                        style: getCustomHeaderStyle(6),
                     });
-                    let bwinf412 = gitgraph.branch("bwinf 41.2");
                     bwinf412.commit("skill learned in bwinf 41.2");
-                    release.commit("");
+                    let humanAndMachine = gitgraph.branch({
+                        name: 'humanAndMachine',
+                        from: develop,
+                    }).commit("");
+                    gitgraph.tag({
+                        name: 'Human & Machine:',
+                        style: getCustomHeaderStyle(7),
+                    });
+                    humanAndMachine.commit("skill learned in Human & Machine");
                     master.commit("");
+                    release.commit("");
                     develop.commit("");
                     tictactoe.commit("");
                     quap.commit("");
                     littSQL.commit("");
                     sudoku.commit("");
                     bwinf411.commit("");
+                    chess4player.commit("");
                     bwinf412.commit("");
+                    humanAndMachine.commit("");
 
                 }}
             </Gitgraph>
