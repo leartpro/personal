6d6d93422644701a5ffc2047acea400bbf3176d9
leartpro <lenni.protte@gmx.de>
2023-03-17 19:23:51 +0100

    Blog adding Project

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {Project} from "../../types/Project";
 import ImageList from '@mui/material/ImageList';
-import {IconButton, ImageListItem, ImageListItemBar, ListSubheader} from "@mui/material";
+import {IconButton, ImageListItem, ImageListItemBar, Typography} from "@mui/material";
 
 //TODO: only use @mui instead of @material-ui
 
@@ -10,10 +10,8 @@ const Blog = ({projects}: { projects: Project[] }) => {
     return (
         <section id={"blog"} style={{marginLeft: '8px', marginRight: '8px'}}>
             <h1>Blog</h1>
+            <Typography>Here are some of my Projects, feel free to explore</Typography>
             <ImageList variant="masonry" cols={3} gap={8}>
-                <ImageListItem key="Subheader" cols={2}>
-                    <ListSubheader component="div">Projects</ListSubheader>
-                </ImageListItem>
                 {projects.map((project: Project) => (
                     <Link to={`/project/${project.id}`} key={project.id}>
                         <ImageListItem key={project.image}>
