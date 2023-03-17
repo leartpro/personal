a08c46fa24c66263601d5ba9c05a44db1bd85755
leartpro <lenni.protte@gmx.de>
2023-03-17 19:17:07 +0100

    Typewriter clean up

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,21 +6,12 @@ import Typewriter from 'react-ts-typewriter';
 
 const About = () => {
     const [text, setText] = useState('');
-    const [currentLineIndex, setCurrentLineIndex] = useState(0);
-    let lines: string[] = text.split("\n");
-
 
     useEffect(() => {
         axios.get('./Content/About.txt').then((response) => {
             setText(response.data);
         });
     }, []);
-
-    const handleTypewriterFinish = () => {
-        if (currentLineIndex < lines.length - 1) {
-            setCurrentLineIndex(currentLineIndex + 1);
-        }
-    };
 
     return (
         <section id="about">
@@ -33,19 +24,6 @@ const About = () => {
                     cursor={true}
                 />
             </Typography>
-            {
-                    lines.slice(0, currentLineIndex + 1).map((line, index) => (
-                        <Typography style={{marginLeft: '10%', marginRight: '10%', whiteSpace: 'pre'}}>
-                            <Typewriter
-                                text={line}
-                                speed={0.1}
-                                loop={false}
-                                cursor={false}
-                                delay={index > 0 ? (lines[index - 1].length) * 10 : 0}
-                                onFinished={index === currentLineIndex ? handleTypewriterFinish : undefined}
-                            />
-                        </Typography>
-                    ))}
         </section>
     );
 }
